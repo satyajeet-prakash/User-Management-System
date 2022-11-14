f190b12d6b28fe9e1191e68a598a362255130d10
Satyajeet Prakash <sant8430@gmail.com>
2022-11-14 13:03:14 +0530

    intial commit

---
+++ user-management-system/src/main/java/org/personal/project/usermanagementsystem/UserManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package org.personal.project.usermanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagementSystemApplication.class, args);
+	}
+
+}
